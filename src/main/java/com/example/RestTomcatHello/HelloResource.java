4da5cc8539f9e9d0f827e3475138d8df726d5f44
Wathsala Vishwajith <wathsala-vishwajith@github.com>
2021-06-03 12:44:37 +0530

    Json 1

--- src/main/java/com/example/RestTomcatHello/HelloResource.java
+++ src/main/java/com/example/RestTomcatHello/HelloResource.java
@@ -12,3 +12,4 @@ public class HelloResource {
         return "Hello, World!";
     }
 }
+
---
+++ src/main/java/com/example/RestTomcatHello/JsonResource.java
@@ -0,0 +1,17 @@
+package com.example.RestTomcatHello;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/json")
+public class JsonResource {
+    @GET
+    @Produces("application/json")
+    public Response response(){
+        return Response.ok("{'status':'ok'}", MediaType.APPLICATION_JSON_TYPE).build();
+    }
+
+}
